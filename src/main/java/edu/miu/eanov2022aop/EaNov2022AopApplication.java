8cece49ebd88cf801a02e1d75f7700e38fed53d5
crisnaxtha <krishnashrestha49@gmail.com>
2022-11-04 14:24:17 -0500

    Inital Commit

---
+++ src/main/java/edu/miu/eanov2022aop/EaNov2022AopApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.eanov2022aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaNov2022AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaNov2022AopApplication.class, args);
+	}
+
+}
